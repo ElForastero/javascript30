236a0772b955d8930701f9a97cea4813aac0b5a9
ElForastero <elforastero@ya.ru>
2017-04-28 20:15:18 +0400

    Day 03 completed

---
+++ Day-03/app.js
@@ -0,0 +1,21 @@
+const handleValueChanging = (name, value) => document.documentElement.style.setProperty(`--${name}`, value);
+const addUnits = (value, unit) => `${value}${unit}`;
+
+const process = (event) => {
+  const { name, division, units } = event.target.dataset;
+  let value = event.target.value;
+
+  if(typeof division !== 'undefined') {
+    value /= parseInt(division, 10);
+  }
+
+  if(typeof units !== 'undefined') {
+    value = addUnits(value, units);
+  }
+
+  handleValueChanging(name, value);
+};
+
+const inputs = document.getElementsByTagName('input');
+Array.prototype.map.call(inputs, input => input.addEventListener('input', process));
+
